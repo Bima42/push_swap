15c0ebdf01d8f190bbf8cc58936a63ed4b5589ee
bimasword <tanpau42@gmail.com>
2021-11-07 15:01:21 +0100

    linked list ok, swap command ok

--- src/push_swap.h
+++ src/push_swap.h
@@ -23,6 +23,8 @@ t_stack     *parsing_string(char **argv);
 t_stack     *parsing_multi_args(char **argv);
 t_stack     *parsing_args(int argc, char **argv);
 
-void    swap(int *stack);
+void    swap(t_stack *a);
+void    swap_both(t_stack *a, t_stack *b);
+int     list_size(t_stack *a);
 
 #endif
--- src/push_swap_tools.c
+++ src/push_swap_tools.c
@@ -1,18 +1,42 @@
 #include "push_swap.h"
 
-void	swap(int *stack)
+void	swap(t_stack *a)
+{
+	t_node	*tmp;
+
+	if (a->front && a->front->next)
+	{
+		tmp = malloc(sizeof(t_node));
+		tmp->data = a->front->next->data;
+		a->front->next->data = a->front->data;
+		a->front->data = tmp->data;
+		free(tmp);
+	}
+	else
+		write(1, "Swap impossible\n", 16);
+}
+
+void	swap_both(t_stack *a, t_stack *b)
+{
+	if (a->front && a->front->next
+			&& b->front && b->front->next)
+	{
+		swap(a);
+		swap(b);
+	}
+	else
+		write(1, "Swap impossible\n", 16);
+}
+
+int	list_size(t_stack *a)
 {
 	int	i;
-	int tmp;
 
 	i = 0;
-	tmp = 0;
-	while (stack[i])
-		i++;
-	if (i > 1)
+	while (a->front)
 	{
-		tmp = stack[0];
-		stack[0] = stack[1];
-		stack[1] = tmp;
+		i++;
+		a->front = a->front->next;
 	}
+	return (i);
 }

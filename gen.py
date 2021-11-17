67f8dcbf2fbfbbedf3beee5227f1616bf70110ee
bimasword <tanpau42@gmail.com>
2021-11-17 18:09:47 +0100

    update

--- gen.py
+++ gen.py
@@ -4,7 +4,7 @@ import random
 file = open("gen.log", "w")
 result = ""
 #Generate 5 random numbers between 10 and 30
-randomlist = random.sample(range(1, 1000), 500)
+randomlist = random.sample(range(0, 2500), 2500)
 for n in randomlist:
     result = f'{result} {n}'
 file.write(result)
